4c49a915046577190d30054c1a6eb5a01d9e8583
Abhishek Ambuskar <abhiambuskar007@gmail.com>
2026-06-10 23:46:42 +0530

    Episode_4

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require("express")
 
 const app = express()
 
+app.get("/tes?ting", (req, res) =>{
+    console.log(req.params)
+    res.send("Testing ? () *")
+})
+
+app.get("/user/:userid/:name/:age",(req, res) =>{
+    console.log(req.params)
+    res.send({firstname: "Abhi", lastname: "Ambuskar"})
+})
+
+
+
+app.post("/user",(req, res) =>{
+    res.send("Data saved to DB successfully")
+})
+
+app.delete("/user",(req, res) =>{
+    res.send("Data deleted from DB")
+})
 
 
 app.use("/test", (req, res) =>{
